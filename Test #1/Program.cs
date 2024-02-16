c1fa55c0b349d72e79926f674cb7445efab1d734
Morkinali <kikiani.nika@gmail.com>
2024-02-17 01:12:31 +0400

    Tebervali 17.24

---
+++ Test #1/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+string secretWord = "giraffe";
+string guess = "";
+int guessCount = 0;
+int guessLimit = 3;
+bool outOfGuesses = false;
+while (guess != secretWord && !outOfGuesses)
+{
+    if (guessCount < guessLimit)
+    {
+
+
+        Console.Write("Enter guess: ");
+        guess = Console.ReadLine();
+        guessCount++;
+    }
+    else
+    {
+        outOfGuesses = true;
+    }
+
+}
+if (outOfGuesses)
+{
+    Console.Write("You Loos !");
+}
+else
+{
+    Console.Write("You Win !");
+}
+
+Console.ReadLine();
